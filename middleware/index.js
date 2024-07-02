523021f209f6c1325b8d5fa6577091ef3b081b11
Aditya-Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2024-07-02 12:44:28 +0530

    Add files via upload

---
+++ middleware/index.js
@@ -0,0 +1,22 @@
+const { listingSchema, reviewSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError");
+
+module.exports.validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, msg);
+  } else {
+    next();
+  }
+};
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, msg);
+  } else {
+    next();
+  }
+};
