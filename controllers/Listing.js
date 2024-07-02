c26440d26c3f72607e537e9f67ace0b83d01db87
Aditya-Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2024-07-02 12:41:47 +0530

    Add files via upload

---
+++ controllers/Listing.js
@@ -0,0 +1,79 @@
+const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new");
+};
+
+module.exports.showListing = async (req, res) => {
+  const listing = await Listing.findById(req.params.id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("Owner");
+  if (!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    res.redirect("/listings");
+  }
+  res.render("listings/show", { listing });
+};
+
+module.exports.createListing = async (req, res) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
+  let url = req.file.path;
+  let filename = req.file.filename;
+  const { listing } = req.body;
+  if (!listing) {
+    req.flash("error", "Invalid listing data!");
+    return res.redirect("/listings/new");
+  }
+
+  const newListing = new Listing(listing);
+  newListing.Owner = req.user._id; // Ensure the owner field matches the schema
+  newListing.image = { url, filename };
+  newListing.geometry = response.body.features[0].geometry;
+
+  let savedListing = await newListing.save();
+  console.log(savedListing);
+  req.flash("success", "New Listing Created!");
+  res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+  res.render("listings/edit", { listing, originalImageUrl });
+};
+
+module.exports.updateListing = async (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body.listing;
+  let listing = await Listing.findByIdAndUpdate(id, updatedData);
+
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
+    await listing.save();
+  }
+  req.flash("success", "Listing Updated!");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req, res) => {
+  await Listing.findByIdAndDelete(req.params.id);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
