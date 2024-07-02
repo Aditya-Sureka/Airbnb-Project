3cdbf51439ff958cdde8ff2455e95aefe2f2db82
Aditya-Sureka <118358669+Aditya-Sureka@users.noreply.github.com>
2024-07-02 12:40:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,152 @@
+// Trial code or working code
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
+const listingRoutes = require("./routes/listings.js");
+const reviewRoutes = require("./routes/reviews.js");
+const userRoutes = require("./routes/user.js");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const app = express();
+const port = 8080;
+
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB:", err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json()); // To parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+const sessionOptions = {
+  store: store,
+  secret:  process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   });
+//   let registeredUser = await User.register(fakeUser, "hellpworld");
+//   res.send(registeredUser);
+// });
+
+app.use(
+  "/listings/css",
+  express.static(path.join(__dirname, "path_to_css_directory"), {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
+
+const validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+app.use("/listings", listingRoutes);
+app.use("/listings/:id/reviews", reviewRoutes);
+app.use("/", userRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found"); // Catch-all for 404 errors
+});
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong!";
+  res.status(statusCode).render("error", { err });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
